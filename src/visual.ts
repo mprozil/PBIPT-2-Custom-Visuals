46405732e35bf3d4df903962ccf42f58796346bb
Margarida Prozil <mprozil@gmail.com>
2019-02-21 22:40:33 +0000

    tag 04 - add visual elements

--- src/visual.ts
+++ src/visual.ts
@@ -27,13 +27,64 @@
 module powerbi.extensibility.visual {
     "use strict";
     export class Visual implements IVisual {
+        private host: IVisualHost;
+        private svg: d3.Selection<SVGElement>;
+        private container: d3.Selection<SVGElement>;
+        private circle: d3.Selection<SVGElement>;
+        private textValue: d3.Selection<SVGElement>;
+        private textLabel: d3.Selection<SVGElement>;
 
         constructor(options: VisualConstructorOptions) {
-           
+            this.svg = d3.select(options.element)
+                .append('svg')
+                .classed('circleCard', true);
+            this.container = this.svg.append("g")
+                .classed('container', true);
+            this.circle = this.container.append("circle")
+                .classed('circle', true);
+            this.textValue = this.container.append("text")
+                .classed("textValue", true);
+            this.textLabel = this.container.append("text")
+                .classed("textLabel", true);
         }
 
         public update(options: VisualUpdateOptions) {
-            
+            let width: number = options.viewport.width;
+            let height: number = options.viewport.height;
+            this.svg.attr({
+                width: width,
+                height: height
+            });
+            let radius: number = Math.min(width, height) / 2.2;
+            this.circle
+                .style("fill", "white")
+                .style("fill-opacity", 0.5)
+                .style("stroke", "black")
+                .style("stroke-width", 2)
+                .attr({
+                    r: radius,
+                    cx: width / 2,
+                    cy: height / 2
+                });
+            let fontSizeValue: number = Math.min(width, height) / 5;
+            this.textValue
+                .text("Value")
+                .attr({
+                    x: "50%",
+                    y: "50%",
+                    dy: "0.35em",
+                    "text-anchor": "middle"
+                }).style("font-size", fontSizeValue + "px");
+            let fontSizeLabel: number = fontSizeValue / 4;
+            this.textLabel
+                .text("Label")
+                .attr({
+                    x: "50%",
+                    y: height / 2,
+                    dy: fontSizeValue / 1.2,
+                    "text-anchor": "middle"
+                })
+                .style("font-size", fontSizeLabel + "px");
         }
     }
 }
