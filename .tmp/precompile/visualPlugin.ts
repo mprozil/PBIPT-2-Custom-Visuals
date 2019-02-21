cee0921f4af163acbca8081258eb566a8cad4505
Margarida Prozil <mprozil@gmail.com>
2019-02-21 22:11:22 +0000

    tag 02 - add d3 and type definitions

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var pBIPTEBC79D0D0C0B439380F061284BF8150F_DEBUG = {
+        name: 'pBIPTEBC79D0D0C0B439380F061284BF8150F_DEBUG',
+        displayName: 'PBIPT',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.3.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.pBIPTEBC79D0D0C0B439380F061284BF8150F.Visual(options),
+        custom: true
+    };
+}
